955777771b7b238d6690331c71f94a4ca21ab3bb
Bruno Wedley <bruno.wsousa@al.infnet.edu.br>
2025-02-14 13:43:52 -0300

    adiciona exercicio 13 resolvido e arquivos necessários para executar o código

--- tp1-13.py
+++ tp1-13.py
@@ -1,2 +1,21 @@
 # Exercício 13: Escrever um dicionário em um arquivo JSON
 # Enunciado: Escreva uma função que recebe um nome de arquivo e um dicionário como entrada e salva os dados no arquivo JSON no formato adequado, garantindo que o conteúdo seja formatado corretamente para fácil leitura.
+
+import json
+
+arquivo = 'arquivo-tp1-13.json'
+dicionario = {
+    "Curso": "Análise e desenvolvimento de sistemas",
+    "Matéria": "Python",
+    "Professor": "Luis Eduardo"
+}
+def salvar_arquivo_json(arquivo_a_salvar, dicionario_recebido):
+    try:
+        with open(arquivo_a_salvar, mode='w', encoding='utf-8') as arquivo:
+            return json.dump(dicionario_recebido, arquivo, ensure_ascii=False, indent=4)
+    except FileNotFoundError:
+        print(f"Arquivo '{arquivo_a_salvar}' não encontrado")
+    except Exception as e:
+        print(f"Erro {e}")
+
+salvar_arquivo_json(arquivo, dicionario)
